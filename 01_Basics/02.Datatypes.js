eaa7aa76e061d8084708bd5c11ec59a336892250
heynikhill <nikhilkmr1725@gmail.com>
2025-09-20 14:21:04 +0000

    datatypes defined

---
+++ 01_Basics/02.Datatypes.js
@@ -0,0 +1,31 @@
+
+"use strict"; //this line is use to  treat all JS code as per the  newer version of js(now it is automatically done)
+
+// alert( 3 + 3) // we are using nodejs, not browser ;this is js code in browser not used in node.js
+
+console.log(3 
+    +
+     3) // code readability should be high ,don't write code in same line it create confusion as in js semicolons in  not mandatory 
+ 
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//Varoius data types are 
+// number => 2 to power 53
+// bigint =>sor very big number like used in stock  market
+// string => ""(perfer) or ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined(datatype of undefined in UNDEFINED)
+console.log(typeof null); // object(datatype of null is OBJECT )
